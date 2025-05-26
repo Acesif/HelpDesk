431fe31f16c24efb12a6e16d817417c7a23f082b
Asif <asifpalak@gmail.com>
2025-05-26 11:36:13 +0600

    allow all origins

--- Server/src/main/java/com/grs/helpdeskmodule/config/WebConfig.java
+++ Server/src/main/java/com/grs/helpdeskmodule/config/WebConfig.java
@@ -10,11 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:4200",                   // Angular local dev
-                        "http://94.250.203.197:4300",              // Production frontend (if correct)
-                        "http://localhost:4300"                    // If 4300 is actually used
-                )
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
