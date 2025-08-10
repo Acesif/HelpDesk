849294612443434d357516d215cbfdb5cbd51543
Asif <asifpalak@gmail.com>
2025-08-10 17:24:55 +0600

    update

--- Client/src/app/shared/ip-config.service.ts
+++ Client/src/app/shared/ip-config.service.ts
@@ -8,8 +8,8 @@ export class ipConfigService {
   constructor() { }
 
   getAddress(): string {
-    return 'http://localhost:7890';
-    // return 'http://94.250.203.197:7890';
+    // return 'http://localhost:7878';
+    return 'http://94.250.203.197:7878';
   }
 
 }
