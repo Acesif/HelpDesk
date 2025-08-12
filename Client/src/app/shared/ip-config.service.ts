17622ffd58efcff3971908ff6db56caf10eff62c
Asif <asifpalak@gmail.com>
2025-08-12 16:01:59 +0600

    change server ip

--- Client/src/app/shared/ip-config.service.ts
+++ Client/src/app/shared/ip-config.service.ts
@@ -8,8 +8,8 @@ export class ipConfigService {
   constructor() { }
 
   getAddress(): string {
-    // return 'http://localhost:7878';
-    return 'http://94.250.203.197:7878';
+    return 'https://training.grs.gov.bd/helpdeskserver';
+    // return 'http://94.250.203.197:7878';
   }
 
 }
